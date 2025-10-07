ae67880f8d20eb03ce757fd07ccc301991b9bada
Feelsky161 <ggooggll03@gmail.com>
2025-10-07 15:10:06 +0300

    обновил код

--- 06.10.py
+++ 06.10.py
@@ -47,34 +47,62 @@ while True:
     if not (playernam== 1 or playernam==2 or playernam==3):
         break
     print("не тупи...")'''
-import random
-pcnam=random.randint(1,3)
-game_count=0
-playerwin=0
-pcwin=0
+'''import random
 
-"""print(pcnam)"""
+game_count=0
+player_win=0
+pc_win=0
+
 while True:
     game_count+=1
-    playernam=int(input("1-камень 2-ножницы 3-бумага: "))
-    if playernam in (1,2,3):
-        break
-    print("не тупи...")
+    pc_number=random.randint(1,3)
+    while True:
+        player_number=int(input("1-камень 2-ножницы 3-бумага: "))
+        if player_number in (1,2,3):
+            break
+        print("не тупи...")
+    pc_str="камень"
+    player_str = "камень"
+    if pc_number==2:
+        pc_str="ножницы"
+    elif pc_number==3:
+        pc_str="бумага"
 
-pcstr= "камень"
-playerstr= "камень"
+    if player_number==2:
+        player_str="ножницы"
+    elif player_number==3:
+        player_str="бумага"
 
-print(f"пк: {pcstr} игрок {playerstr}")
-if playernam==pcnam:
-    print("ничья")
-elif playernam==1:
-    print("пк победил" if pcnam==3 else "пк проиграл")
-elif playernam==2:
-    print("пк победил" if pcnam==1 else "пк проиграл")
-elif playernam ==3:
-    print("пк победил" if pcnam ==2 else "пк проиграл")
+    print(f"пк: {pc_str} игрок: {player_str}")
+    if player_number==pc_number:
+        print("ничья")
+    elif player_number==1:
+        if pc_number == 3:
+            print("пк победил")
+            pc_win+=1
+        else:
+            print("пк проиграл")
+            player_win+=1
+    elif player_number==2:
+        if pc_number == 1:
+            print("пк победил")
+            pc_win+=1
+        else:
+            print("пк проиграл")
+            player_win+=1
+    elif player_number==3:
+        if pc_number == 2:
+            print("пк победил")
+            pc_win+=1
+        else:
+            print("пк проиграл")
+            player_win+=1
 
-    again=input("введите 1 если хотите продолжить: ")
+    print(f"побед пк: {pc_win} побед игрока: {player_win} ничьи:{game_count-pc_win-player_win}")
+    again = input("введите 1 если хотите сыграть еще раз: ")
     if again!="1":
-        breakpoint()
+        break
     print()
+
+print(f"сыграно игр {game_count}")
+'''
