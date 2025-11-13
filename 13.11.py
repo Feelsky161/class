2cabb514727ea677ce7281914612f6c4f89e618b
Feelsky161 <ggooggll03@gmail.com>
2025-11-13 21:14:58 +0300

    работа в классе

---
+++ 13.11.py
@@ -0,0 +1,52 @@
+"""class humon:
+    def __init__(self,name,color,group):
+    self.name=name
+    self.age=age
+
+if __name__ == '__main__':
+
+"""
+"""    cats = [
+        {
+            "имя": "Мисс Пеппер",
+            "порода": "мейн-кун",
+            "окрас": "трёхцветный (калико)"
+        },
+        {
+            "имя": "Серафина",
+            "порода": "британская короткошёрстная",
+            "окрас": "чёрный блестящий"
+        },
+        {
+            "имя": "Тоторо",
+            "порода": "японский бобтейл",
+            "окрас": "черепаховый (торти)"
+        }
+    ]
+
+    for i, cat in enumerate(cats, start=1):
+        print(f"{i}. {cat['имя']} — {cat['порода']}, окрас: {cat['окрас']}")
+"""
+"""class Humon:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def show_info(self):
+    print(f"name: "{self.name}\n+
+        f"name: "{self.name}\n+)\n+
+        f"name: "{self.name}\n+)
+
+
+if __name__ == '__main__':
+    humon1 = Humon("Коля",22)
+    humon2 = Humon("Петя",23)
+    humon3 = Humon("Вася",24)"""
+"""class Chel:
+    def __init__(self,age):
+        self.age=age
+
+    def show_info(self):
+if __name__ == '__main__':
+            humon1 = Humon("Коля", 22)
+            humon2 = Humon("Петя", 23)
+            humon3 = Humon("Вася", 24)"""
